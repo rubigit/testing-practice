af9236962114a1830fb494c54d4fad70e8dac03e
rubigit <rcj_88@hotmail.com>
2021-03-29 19:49:40 -0400

    Update 02-difference, Add message when all test passed successfully

--- 01-unit/02-difference.js
+++ 01-unit/02-difference.js
@@ -36,6 +36,7 @@ try {
   var result = difference();
   if (result !== 0) throw new Error('Expected difference() to be 0. Received: ' + result);
 
+  console.log('All tests passed successfully.');
   // ==================================================
   // PRINT ERRORS
   // ==================================================
