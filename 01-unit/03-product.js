5a30ee1f4c6236716df4e550205affd29755e91d
rubigit <rcj_88@hotmail.com>
2021-03-29 19:57:41 -0400

    Update 03-product, Introduce test case to ignore additional numbers

--- 01-unit/03-product.js
+++ 01-unit/03-product.js
@@ -25,7 +25,8 @@ try {
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
-
+  var result = product(2, 2, 2);
+  if (result !== 4) throw new Error('Expected product(2, 2, 2) to be 4. Received: ' + result);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
