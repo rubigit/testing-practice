5e8f7b3165a54b6706e56ca8d91983c54420c3f7
rubigit <rcj_88@hotmail.com>
2021-03-29 21:59:40 -0400

    Update 01-calc.js, Introduce test case when the user provides:divide

--- 02-integration/01-calc.js
+++ 02-integration/01-calc.js
@@ -59,7 +59,8 @@ try {
   // Test Case 4
   // --------------------------------------------------
   // It should return the correct quotient when the user provides: 'divide', 9, 3.
-
+  var result = calc('divide', 9, 3);
+  if (result !== 3) throw new Error('Expected calc("divide", 9, 3) to be 3. Received: ' + result);
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
