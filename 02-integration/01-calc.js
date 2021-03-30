1b9e2cc0db37dcdc4d939b9ba661c4034ffa0ada
rubigit <rcj_88@hotmail.com>
2021-03-29 21:56:31 -0400

    Update 01-calc.js, Introduce test case when the user provides:subtract

--- 02-integration/01-calc.js
+++ 02-integration/01-calc.js
@@ -47,7 +47,8 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return the correct difference when the user provides: 'subtract', 20, 10.
-
+  var result = calc('subtract', 20, 10);
+  if (result !== 10) throw new Error('Expected calc("subtract", 20, 10) to be 10. Received: ' + result);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
